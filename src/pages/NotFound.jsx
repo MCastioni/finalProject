f9508365d1b7481e0a352ab79bf152d772b7f607
micaelacastioni@gmail.com <micaelacastioni@gmail.com>
2025-08-14 23:07:57 -0300

    edit: styles to Not Found Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,11 +4,15 @@ import { Layout } from "../components/Layout"
 const NotFound = () => {
   return (
     <Layout>
-      <h1>Página no encontrada</h1>
-      <img src="notFoundCat.jpeg" alt="Page not found, sad cat" />
-      <p>¡Ups! Parece que la página que estás buscando no existe.</p>
-      <p> Por favor, verifica la URL o vuelve al inicio.</p>
-      <Link to="/">Ir a inicio</Link>
+      <h1 id="notTitle">Página no encontrada</h1>
+      <section id="notFoundPage">
+      <div id="notFoundContainer">
+      <img id="notImg" src="notFoundCat.jpeg" alt="Page not found, sad cat" />
+      <p id="notText">¡Ups! Parece que la página que estás buscando no existe.</p>
+      <p id="notText"> Por favor, verifica la URL o vuelve al inicio.</p>
+      <Link id="notLink" to="/">Ir a inicio</Link>
+      </div>  
+      </section> 
     </Layout>
   )
 }
